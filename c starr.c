d3272d2e71b05c03181e3ae359af5df162f61d83
Shaziafayaz <shaziadagga@gmail.com>
2025-06-28 15:19:33 +0530

    Add files via upload

---
+++ c starr.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+ {
+int i, j;
+int rows = 5;
+for (i = 1; i <= rows; i++)
+    {
+for （j=1；j<=i;j++）
+｛
+if(i = 1 || i == rows 1| j==1|| j==i)
+printf ("* ");
+else
+printf(" ");
+｝
+printf("\n");
+    }
+return 0;
+ }
